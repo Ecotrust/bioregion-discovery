1cd5c63a2cc4d0d5745233d8a56a2c374a3c6fcd
sfletche <sfletche@gmail.com>
2011-07-01 12:25:38 -0700

    forgot to add migration script

---
+++ bmm/analysis/migrations/0003_auto__chg_field_languages_name_prop.py
@@ -0,0 +1,60 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Changing field 'Languages.name_prop'
+        db.alter_column('analysis_languages', 'name_prop', self.gf('django.db.models.fields.CharField')(max_length=40, null=True))
+
+
+    def backwards(self, orm):
+        
+        # Changing field 'Languages.name_prop'
+        db.alter_column('analysis_languages', 'name_prop', self.gf('django.db.models.fields.CharField')(max_length=40))
+
+
+    models = {
+        'analysis.ecoregions': {
+            'Meta': {'object_name': 'EcoRegions'},
+            'area': ('django.db.models.fields.FloatField', [], {}),
+            'area_km2': ('django.db.models.fields.IntegerField', [], {}),
+            'eco_code': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'eco_name': ('django.db.models.fields.CharField', [], {'max_length': '99'}),
+            'geometry': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {'srid': '54009', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'perimeter': ('django.db.models.fields.FloatField', [], {}),
+            'rangeland': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'realm': ('django.db.models.fields.CharField', [], {'max_length': '3', 'null': 'True', 'blank': 'True'})
+        },
+        'analysis.languages': {
+            'Meta': {'object_name': 'Languages'},
+            'c1': ('django.db.models.fields.CharField', [], {'max_length': '33'}),
+            'cnt': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'family': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
+            'familyprop': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
+            'g': ('django.db.models.fields.CharField', [], {'max_length': '225', 'null': 'True', 'blank': 'True'}),
+            'geometry': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {'srid': '54009', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lmp_c1': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'lmp_class': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
+            'lmp_pop1': ('django.db.models.fields.FloatField', [], {}),
+            'nam_ansi': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'nam_label': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'name2': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'name_prop': ('django.db.models.fields.CharField', [], {'max_length': '40', 'null': 'True', 'blank': 'True'}),
+            'pop': ('django.db.models.fields.CharField', [], {'max_length': '86', 'null': 'True', 'blank': 'True'})
+        },
+        'analysis.reportcache': {
+            'Meta': {'object_name': 'ReportCache'},
+            'context': ('picklefield.fields.PickledObjectField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'wkt_hash': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['analysis']
