aff8f25827fcfea37ac386f02e058aea0ee1c810
sfletche <sfletche@gmail.com>
2011-07-07 17:18:05 -0700

    little things

--- bmm/mybioregions/urls.py
+++ bmm/mybioregions/urls.py
@@ -3,6 +3,5 @@ from views import *
 
 urlpatterns = patterns('',
     (r'^analysis/', include('analysis.urls')),
-    #url(r'shapefile/mpa/(?P<instances>(\d+,?)+)/$', convert_to_shp, name='mpa_shapefile'),
 
 )
