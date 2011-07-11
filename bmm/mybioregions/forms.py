d794c3175c9a6d4d1243c7eee46e84c09607fd55
Matthew Perry <mperry@ecotrust.org>
2011-07-11 08:35:05 -0700

    Iterate over max_cost to converge on the optimal value for acheiving a desired bioregion size

--- bmm/mybioregions/forms.py
+++ bmm/mybioregions/forms.py
@@ -10,17 +10,17 @@ class MyBioregionForm(FeatureForm):
     #might change the following to ModelChoiceField to pull city names from a model
     #also, might use different strategy altogether that allows users to simply select a point on a map as the starting point
     input_starting_point = forms.CharField(label="Bioregion Starting Point", widget=SimplePoint(title="Bioregion"))
-    input_temp_weight = forms.FloatField(min_value=0, max_value=1.0, initial=0.5,
-            widget=SliderWidget(min=0,max=1,step=0.01,image='bmm/img/sun.png'),
+    input_temp_weight = forms.FloatField(min_value=0, max_value=100.0, initial=25,
+            widget=SliderWidget(min=0,max=100,step=1,image='bmm/img/sun.png'),
             label="Value given to Temperature")
-    input_language_weight = forms.FloatField(min_value=0, max_value=1.0, initial=0.5,
-            widget=SliderWidget(min=0,max=1,step=0.01,image='bmm/img/lang.png'),
+    input_language_weight = forms.FloatField(min_value=0, max_value=100.0, initial=25,
+            widget=SliderWidget(min=0,max=100,step=1,image='bmm/img/lang.png'),
             label="Value given to Spoken Language")
-    input_precip_weight = forms.FloatField(min_value=0, max_value=1.0, initial=0.5,
-            widget=SliderWidget(min=0,max=1,step=0.01,image='bmm/img/rain.png'),
+    input_precip_weight = forms.FloatField(min_value=0, max_value=100.0, initial=25,
+            widget=SliderWidget(min=0,max=100,step=1,image='bmm/img/rain.png'),
             label="Value given to Precipitation")
-    input_biomass_weight = forms.FloatField(min_value=0, max_value=1.0, initial=0.5,
-            widget=SliderWidget(min=0,max=1,step=0.01,image='bmm/img/veg.png'),
+    input_biomass_weight = forms.FloatField(min_value=0, max_value=100.0, initial=25,
+            widget=SliderWidget(min=0,max=100,step=1,image='bmm/img/veg.png'),
             label="Value given to Vegetation")
         
     class Meta(FeatureForm.Meta):
--- bmm/mybioregions/models.py
+++ bmm/mybioregions/models.py
@@ -32,15 +32,16 @@ class MyBioregion(Analysis):
         from lingcod.analysistools.grass import Grass
 
         coords = self.input_starting_point.transform(settings.GEOMETRY_DB_SRID, clone=True)
-        max_cost=100 * (self.input_temp_weight + self.input_language_weight + \
-                        self.input_precip_weight + self.input_biomass_weight)
+        p_temp = self.input_temp_weight
+        p_precip = self.input_precip_weight
+        p_language = self.input_language_weight
+        p_biomass = self.input_biomass_weight
 
         g = Grass('world_moll', 
                 gisbase="/usr/local/grass-6.4.1RC2", 
                 gisdbase="/home/grass",
                 autoclean=True)
-        g.verbose = True
-        g.run('g.region rast=soilmoist')
+        g.verbose = False
         rasts = g.list()['rast']
 
         outdir = '/tmp'
@@ -49,35 +50,58 @@ class MyBioregion(Analysis):
         if os.path.exists(output):
             raise Exception(output + " already exists")
 
-        g.run('r.mapcalc "weighted_combined_slope =  0.01 + ' +
-                            '(%s * temp_slope) + ' % self.input_temp_weight  + 
-                            '(%s * lang_slope) + ' % self.input_language_weight  +
-                            '(%s * precip_slope) + ' % self.input_precip_weight +
-                            '(%s * biomass_slope)' % self.input_biomass_weight +
+
+        # Guess seed value
+        x = (p_temp + p_language + p_precip + p_biomass + 0.5) 
+        t_weight = ((14.7965 * x ) ** 0.7146) 
+        const1 = 2000
+        max_cost = x * const1 / t_weight
+
+        desired_size_mHa = 100 #million Hectares
+        desired_size = 10000000000 * desired_size_mHa
+
+        # set initial region
+        g.run('g.region rast=soilmoist')
+        g.run('r.mapcalc "weighted_combined_slope = 0.5 + ' +
+                            '(%s * temp_slope) + ' % p_temp  + 
+                            '(%s * lang_slope) + ' % p_language +
+                            '(%s * precip_slope) + ' % p_precip +
+                            '(%s * biomass_slope)' % p_biomass +
                             '"')
-        g.run('r.rescale input=weighted_combined_slope output=wcr_slope to=0,100')
-        g.run('r.cost -k input=wcr_slope output=cost coordinate=%s,%s max_cost=%s' % \
-                (coords[0],coords[1],max_cost) )
-        g.run('r.mapcalc "bioregion=if(cost >= 0)"')
-        g.run('r.to.vect -s input=bioregion output=bioregion_poly feature=area')
-        g.run('v.out.ogr -c input=bioregion_poly type=area format=GeoJSON dsn=%s' % output)
 
-        from django.contrib.gis.gdal import DataSource
-        ds = DataSource(output)
-        layer = ds[0]
-        geom = layer[0].geom.geos
+        ################# Run #2 - adjusted cost #####################
+        ratio = 0.0
+        i = 0
+        overs = []
+        unders = []
+        while ratio < 0.9 or ratio > 1.1:
+            g.run('g.region n=7078485 s=3660936 e=-7024061 w=-10560592')
+            g.run('r.cost -k input=weighted_combined_slope output=cost1 coordinate=%s,%s max_cost=%s' % \
+                    (coords[0],coords[1],max_cost) )
+            g.run('r.mapcalc "bioregion1=if(cost1 >= 0)"')
+            g.run('r.to.vect input=bioregion1 output=bioregion1_poly feature=area') # -s
+            if os.path.exists(output):
+                os.remove(output)
+            g.run('v.out.ogr -c input=bioregion1_poly type=area format=GeoJSON dsn=%s' % output)
+            largest_area, geom = get_largest_from_json(output)
 
-        # Take the single polygon with the largest geometry 
-        # Assume the rest are slivers, etc
-        largest_area = geom.area
-        for feat in layer[1:]:
-            if feat.geom.area > largest_area:
-                largest_area = feat.geom.area
-                geom = feat.geom.geos
+            ratio = desired_size/largest_area
+            if ratio < 1.0 : 
+                overs.append(max_cost)
+            else:
+                unders.append(max_cost)
+
+            print "# run #", i, "ratio", int(ratio*100),"%", "max cost", int(max_cost)
+            print "   # actual size", int((largest_area/10000.0)/1000000.0)
+            print "   # desired_size", int((desired_size/10000.0)/1000000.0)
+            try:
+                max_cost = (max(unders) + min(overs))/2.0
+            except ValueError:
+                max_cost = max_cost * (ratio ** 0.5)
+            i = i+1
+
 
         geom.srid = settings.GEOMETRY_DB_SRID 
-        g2 = geom.buffer(20000)
-        geom = g2.buffer(-20000)
         if geom and not settings.DEBUG:
             os.remove(output)
             del g
@@ -97,7 +121,7 @@ class MyBioregion(Analysis):
                 if orig._get_FIELD_display(f) != getattr(self,f.name):
                     rerun = True
                     break
-        super(MyBioregion, self).save(rerun=rerun, *args, **kwargs) 
+        super(MyBioregion, self).save(rerun=rerun)
 
     @classmethod
     def mapnik_geomfield(self):
@@ -205,3 +229,18 @@ class Placeholder(PolygonFeature):
         form = 'mybioregions.forms.PlaceholderForm'
         show_template = 'placeholder/show.html'
       
+
+def get_largest_from_json(output):
+    from django.contrib.gis.gdal import DataSource
+    ds = DataSource(output)
+    layer = ds[0]
+    geom = layer[0].geom.geos
+
+    # Take the single polygon with the largest geometry 
+    # Assume the rest are slivers, etc
+    largest_area = geom.area
+    for feat in layer[1:]:
+        if feat.geom.area > largest_area:
+            largest_area = feat.geom.area
+            geom = feat.geom.geos
+    return largest_area, geom
