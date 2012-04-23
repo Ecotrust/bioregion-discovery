34f660a05315341e39fa282abf5fbabaf37f513f
Matthew Perry <mperry@ecotrust.org>
2012-04-23 14:27:36 -0700

    form class spacing

--- bmm/mybioregions/forms.py
+++ bmm/mybioregions/forms.py
@@ -7,10 +7,10 @@ from django.contrib.gis.geos import fromstr
 #from django.forms.widgets import RadioSelect
 from models import *
 
+
 class MyBioregionForm(FeatureForm):
-    #might change the following to ModelChoiceField to pull city names from a model
-    #also, might use different strategy altogether that allows users to simply select a point on a map as the starting point
-    input_starting_point = forms.CharField(label="Bioregion Starting Point", widget=SimplePoint(title="Bioregion"))
+    input_starting_point = forms.CharField(label="Bioregion Starting Point", 
+            widget=SimplePoint(title="Bioregion"))
     input_temp_weight = forms.FloatField(min_value=0, max_value=100.0, initial=50,
             widget=SliderWidget(min=0,max=100,step=1, show_number=False, image='bmm/img/sun.png'),
             label="Temperature")
@@ -29,9 +29,6 @@ class MyBioregionForm(FeatureForm):
     input_marine_weight = forms.FloatField(min_value=0, max_value=100.0, initial=0,
             widget=SliderWidget(min=0,max=100,step=1, show_number=False, image='bmm/img/marine.png'),
             label="Marine Environment")
-    #input_dwater_weight = forms.FloatField(min_value=0, max_value=100.0, initial=50,
-    #        widget=SliderWidget(min=0,max=100,step=1, show_number=False, image='bmm/img/dwater.png'),
-    #        label="Proximity to Major Waterbodies")
         
     class Meta(FeatureForm.Meta):
         model = MyBioregion
@@ -39,13 +36,16 @@ class MyBioregionForm(FeatureForm):
         for f in model.output_fields():
             exclude.append(f.attname)
 
+
 class FolderForm(FeatureForm):
     class Meta(FeatureForm.Meta):
         model = Folder
     
+
 class DrawnBioregionForm(SpatialFeatureForm):
     class Meta(SpatialFeatureForm.Meta):
         model = DrawnBioregion
+
 
 class StoryPointForm(SpatialFeatureForm):
     class Meta(SpatialFeatureForm.Meta):
